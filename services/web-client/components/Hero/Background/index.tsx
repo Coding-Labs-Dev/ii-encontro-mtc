76fefe53a87e14f0b05756129c6341641dea9dcf
Luis Ramirez <luis@codinglabs.dev>
2020-08-01 22:55:02 -0300

    feat/add bem helper

--- services/web-client/components/Hero/Background/index.tsx
+++ services/web-client/components/Hero/Background/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import bemHelper from 'lib/bem';
 
 import styles from './Background.module.scss';
 
+const bem = bemHelper(styles, 'HeroBackground');
+
 const Background: React.FC = ({ children }) => {
   return (
-    <div className={styles['b-HeroBackground']}>
+    <div className={bem.b()}>
       {children}
-      <div className={styles['b-HeroBackground__texture']} />
+      <div className={bem.el('texture')} />
     </div>
   );
 };
---
+++ services/web-client/lib/bem.ts
@@ -0,0 +1,52 @@
+const unique = (arr: Array<any>) => [...new Set(arr)];
+
+export class BEM {
+  readonly stylesheet: { [key: string]: string };
+  readonly bemBase: string;
+
+  constructor(stylesheet: { [key: string]: string }, bemBase: string) {
+    this.stylesheet = stylesheet;
+    this.bemBase = bemBase;
+  }
+
+  getStyles(classes: Array<string>): Array<string> {
+    return classes.map((className) => this.stylesheet[className]);
+  }
+
+  blockName() {
+    return `bem-${this.bemBase}`;
+  }
+
+  // b-Block
+  b(...rest: any[]) {
+    return this.getStyles(unique([this.blockName(), ...rest]));
+  }
+
+  // b-Block__Element
+  el(elName: string, ...rest: any[]) {
+    return this.getStyles(unique([`${this.blockName()}__${elName}`, ...rest]));
+  }
+
+  // b-Block--Modifier
+  mod(modName: string, ...rest: any[]) {
+    return this.getStyles(
+      unique([this.b(), `${this.blockName()}--${modName}`, ...rest])
+    );
+  }
+
+  // b-Block__Element--Modifier
+  elMod(elName: string, modName: string, ...rest: any[]) {
+    return this.getStyles(
+      unique([
+        this.el(elName),
+        `${this.blockName()}__${elName}--${modName}`,
+        ...rest,
+      ])
+    );
+  }
+}
+
+const bem = (stylesheet: { [key: string]: string }, bemBase: string) =>
+  new BEM(stylesheet, bemBase);
+
+export default bem;
