773ddfcab2093040c57df3cc8400afb5750ccd7b
Luis Ramirez <luis@codinglabs.dev>
2020-08-29 01:18:53 -0300

    feat/web-client: update database

--- services/web-client/lib/i18n/messages.ts
+++ services/web-client/lib/i18n/messages.ts
@@ -22,7 +22,7 @@ const messages = {
   PaymentPlans: {
     Title: 'Investimento',
     Footer:
-      'Hospedagem por conta do aluno. Para reservas nos hoteis sugeridos, utilize o código {coupon}. Inscrições realizadas até {date} poderão ser parceladas no cartão de crédito em até {before} vezes. Após {date}, poderão ser parceladas em até {after} vezes. Não haverá inscrições no local. Vagas Limitadas.',
+      'Hospedagem por conta do aluno. Para reservas nos hoteis sugeridos, utilize o código {coupon}. Inscrições poderão ser parceladas em até {after} vezes. Não haverá inscrições no local. Vagas Limitadas.',
   },
   Institutes: {
     Title: 'Apoio Institucional',
